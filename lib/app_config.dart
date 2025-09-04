525d5e7c1f47143760748fa04db4e8760cc4edcc
pa596 <oliver.lu@pa.com.sg>
2025-09-04 15:27:12 +0800

    misc

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -13,8 +13,8 @@ const bool loadDashboard = true;
 // const bool useDevOresvc = true;
 // use dev usersvc in release mode
 // const bool useDevUsersvc = true;
-const DeploymentTeir oreSvcTargetTier = DeploymentTeir.prod;
-const DeploymentTeir userSvcTargetTier = DeploymentTeir.prod;
+const DeploymentTeir oreSvcTargetTier = DeploymentTeir.dev;
+const DeploymentTeir userSvcTargetTier = DeploymentTeir.dev;
 
 // setting here is used to hard limit the scope of the portal
 // when deploying to different environment.
